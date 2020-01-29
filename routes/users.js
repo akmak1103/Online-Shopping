3717a7588e816c49b195312e02dab9b9ac72284a
Akshay Makkar <akshaym1103@gmail.com>
2020-01-29 10:09:23 +0530

    Tried making new route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/akshay', function(req, res, next) {
+  res.send('Welcome Akshay! Hope you are doing good today!!');
+});
+
 module.exports = router;
